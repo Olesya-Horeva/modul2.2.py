d898a3259924af1766c13501044f1ab72f7fe2e8
Olesya-Horeva <kushnerova.olesya.1994@mail.ru>
2024-06-30 23:22:05 +0300

    Add files via upload

---
+++ modul2.2.py
@@ -0,0 +1,10 @@
+first = int(input('Введите первое число: '))
+second = int(input('Введите второе число: '))
+third = int(input('Введите третье число: '))
+if first == second == third:
+    print("Равны: 3 числа")
+elif first == second or first == third:
+    print('Равны: 2 числа')
+else:
+    print('Равных чисел нет')
+
